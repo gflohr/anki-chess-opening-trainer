9153ab24fd5661aaddb7cd4cdad1ad19ee94674c
Guido Flohr <guido.flohr@cantanea.com>
2023-11-09 14:18:41 +0100

    implement skeleton for GUI add-on

---
+++ __init__.py
@@ -0,0 +1,35 @@
+import gettext
+import os
+import anki
+from aqt import mw
+from aqt.utils import qconnect
+from aqt.qt import QAction, QDialog
+
+
+def showImportDialog() -> None:
+	dlg = QDialog(mw)
+	dlg.setWindowTitle(_('Opening Trainer'))
+	dlg.exec()
+
+def initI18N() -> None:
+	supported = ['en', 'de']
+	lang = anki.lang.current_lang
+	if not lang in supported:
+		lang = supported[0]
+
+	localedir = os.path.join(os.path.dirname(__file__), 'locale')
+	t = gettext.translation(
+		'opening-trainer',
+		localedir=localedir,
+		languages=[lang])
+	t.install()
+
+def addMenuItem():
+	action = QAction(_('Opening Trainer'), mw)
+	# set it to call testFunction when it's clicked
+	qconnect(action.triggered, showImportDialog)
+	# and add it to the tools menu
+	mw.form.menuTools.addAction(action)
+
+initI18N()
+addMenuItem()
