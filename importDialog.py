ee0c471fbf241faf8bea1e58bc837f0f0cc4b065
Guido Flohr <guido.flohr@cantanea.com>
2023-11-13 16:43:38 +0100

    preserve dialog state

--- importDialog.py
+++ importDialog.py
@@ -18,55 +18,56 @@ import sys
 class _Config():
 	def __init__(self):
 		config = mw.addonManager.getConfig(__name__)
-		print(f'loaded: {config}', file=sys.stderr)
 		col = mw.col
 
 		if('colour' in config
-		   and "<class 'str'>" == type(config['colour'])
+		   and isinstance(config['colour'], str)
 		   and 'black' == config['colour']):
 			self.colour = 'black'
 		else:
 			self.colour = 'white'
 		
-		if 'notetype' in config and "<class 'str'" == type(config['notetype']):
+		if 'notetype' in config and isinstance(config['notetype'], str):
 			notetype = config['notetype']
 		else:
 			notetype = 'Basic'
-		if notetype in col.models.all():
+		if notetype in list(map(lambda m: m['name'], col.models.all())):
 			self.notetype = notetype
 		else:
 			self.notetype = None
 		
 		self.files = {'white': [], 'black': []}
-		if('files' in config and "<class 'dict'>" == type(config['notetype'])):
+		if 'files' in config and isinstance(config['files'], dict):
 			files = config['files']
-			if 'white' in files and "<class 'list'>" == type(config['files']):
+			if 'white' in files and isinstance(config['files']['white'], list):
+				self.files['white'] = []
 				for filename in files['white']:
-					if "<class 'str'>" == type(filename):
+					if isinstance(filename, str):
 						self.files['white'].append(filename)
-			if 'black' in files and "<class 'list'>" == type(config['files']):
-				for filename in files['white']:
-					if "<class 'str'>" == type(filename):
+			if 'black' in files and isinstance(config['files']['black'], list):
+				self.files['black'] = []
+				for filename in files['black']:
+					if isinstance(filename, str):
 						self.files['black'].append(filename)
 
 		self.decks = {'white': None, 'black': None}
-		if('decks' in config and "<class 'dict'>" == type(config['decks'])):
+		if 'decks' in config and isinstance(config['decks'], dict):
 			decks = config['decks']
 			if('white' in decks
-			   and "<class 'str'>" == type(decks['white'])
-			   and decks['white'] in col.decks.all()):
+			   and isinstance(decks['white'], str)
+			   and decks['white'] in list(map(lambda d: d['name'], col.decks.all()))):
 				self.decks['white'] = decks['white']
 			if('black' in decks
-			   and "<class 'str'>" == type(decks['black'])
-			   and decks['black'] in col.decks.all()):
+			   and isinstance(decks['black'], str)
+			   and decks['black'] in list(map(lambda d: d['name'], col.decks.all()))):
 				self.decks['black'] = decks['black']
 
 	def save(self, dlg: QDialog) -> None:
 		colourIndex = dlg.colourCombo.currentText()
-		if colourIndex == 0:
-			colour = 'white'
-		else:
+		if colourIndex == 1:
 			colour = 'black'
+		else:
+			colour = 'white'
 		self.colour = colour
 		self.notetype = dlg.modelCombo.currentText()
 		self.files[colour] = []
@@ -80,7 +81,6 @@ class _Config():
 			'decks': self.decks,
 		}
 
-		print(f'saving: {config}', file=sys.stderr)
 		mw.addonManager.writeConfig(__name__, config)
 
 class ImportDialog(QDialog):
@@ -115,6 +115,7 @@ class ImportDialog(QDialog):
 		self.layout.addWidget(self.colourCombo, 2, 1)
 		self.colourCombo.addItem(_('White'))
 		self.colourCombo.addItem(_('Black'))
+		self.colourCombo.currentIndexChanged.connect(self._colourChanged)
 
 		self.layout.addWidget(QLabel(_('Note type')), 3, 0)
 		self.modelCombo = QComboBox()
@@ -139,14 +140,30 @@ class ImportDialog(QDialog):
 		self.buttonBox.rejected.connect(self.reject)
 		self._fillDialog()
 
+	def _colourChanged(self) -> None:
+		if self.colourCombo.currentIndex() == 1:
+			colour = 'black'
+		else:
+			colour = 'white'
+		config = self.config
+
+		wanted = config.decks[colour]
+		for i in range(self.deckCombo.count()):
+			if wanted == self.deckCombo.itemText(i):
+				self.deckCombo.setCurrentIndex(i)
+				break
+
+		self.fileList.clear()
+		for filename in config.files[colour]:
+			self.fileList.addItem(filename)
+
 	def _fillDialog(self) -> None:
 		config = self.config
-		print(config, file=sys.stderr)
 		colour = config.colour
-		if 'white' == colour:
-			self.colourCombo.setCurrentIndex(0)
-		else:
+		if 'black' == colour:
 			self.colourCombo.setCurrentIndex(1)
+		else:
+			self.colourCombo.setCurrentIndex(0)
 
 		if config.decks[colour] != None:
 			wanted = config.decks[colour]
@@ -154,7 +171,7 @@ class ImportDialog(QDialog):
 				if wanted == self.deckCombo.itemText(i):
 					self.deckCombo.setCurrentIndex(i)
 					break
-		
+
 		if config.notetype != None:
 			wanted = config.notetype
 			for i in range(self.modelCombo.count()):
@@ -162,6 +179,7 @@ class ImportDialog(QDialog):
 					self.modelCombo.setCurrentIndex(i)
 					break
 
+		self.fileList.clear()
 		for filename in config.files[colour]:
 			self.fileList.addItem(filename)
 
