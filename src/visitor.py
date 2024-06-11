cf68c6a68e081dc6a343cb60abf4475d54e8f445
Guido Flohr <guido.flohr@cantanea.com>
2024-06-11 18:05:56 +0300

    return early
    This is still not fully understood but without the return, the vast
    majority of cards do not get an answer.

--- src/visitor.py
+++ src/visitor.py
@@ -87,10 +87,12 @@ class PositionVisitor(BaseVisitor):
 						question.add_comment(comment)
 						self.accumulated_comments = []
 				question.set_board(board.copy())
-			elif not answer.find(self.cards[text].answers):
-				self.cards[text].add_answer(answer)
-				self.my_move = True
-				self.last_text = text
+			elif answer.find(self.cards[text].answers):
+				return board
+
+			self.cards[text].add_answer(answer)
+			self.my_move = True
+			self.last_text = text
 		else:
 			self.accumulated_comments = []
 			self.my_move = False
