39259deb3dd8d31e0a8957bf601a8fad27631323
Guido Flohr <guido.flohr@cantanea.com>
2024-07-24 10:41:02 +0300

    move 2D piece CSS

--- tools/get-lichess-assets.sh
+++ tools/get-lichess-assets.sh
@@ -6,10 +6,10 @@ rm -rf lila
 
 git clone --depth=1 https://github.com/lichess-org/lila
 
-rm -f assets/css/pieces/*.css
-test -e assets/css/pieces || mkdir -p assets/css/pieces
-cp -r lila/public/piece-css/*.css assets/css/pieces
-rm -f assets/css/pieces/*.external.css
+rm -f assets/css/piece/*.css
+test -e assets/css/piece || mkdir -p assets/css/piece
+cp -r lila/public/piece-css/*.css assets/css/piece
+rm -f assets/css/piece/*.external.css
 
 rm -f assets/images/2d/board/*.jpg
 rm -f assets/images/2d/board/svg/*.svg
@@ -20,5 +20,29 @@ rm -f assets/images/2d/board/*.orig.jpg
 rm -rf assets/images/3d/*
 test -e assets/images/3d || mkdir -p assets/images/3d
 cp -r lila/public/images/staunton/* assets/images/3d
+
+# Thumbnails for settings dialogue.
+rm -f src/images/2d/piece/*
+for dir in lila/public/piece/*; do
+	piece=`basename $dir`
+	test $piece != mono && cp $dir/bN.svg src/images/2d/piece/$piece.svg
+done
+rm -f src/images/2d/piece/mono.svg
+
+rm -f src/images/2d/board/*
+for thumb in assets/images/2d/board/*.thumbnail.*; do
+	cp $thumb src/images/2d/board
+done
+
+rm -f src/images/3d/piece/*
+for dir in assets/images/3d/piece/*; do
+	piece=`basename $dir`
+	cp $dir/Black-Knight.png src/images/3d/piece/$piece.png
+done
+
+rm -f src/images/3d/board/*
+for thumb in assets/images/3d/board/*.thumbnail.*; do
+	cp $thumb src/images/3d/board
+done
 
 rm -rf lila
